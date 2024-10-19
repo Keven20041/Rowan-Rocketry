f3d9d2fd623f236c15bf92dca6ce8254af11389d
Keven20041 <116309709+Keven20041@users.noreply.github.com>
2024-10-18 20:16:54 -0400

    Added a function to help change tabs smoothly

--- script.js
+++ script.js
@@ -27,3 +27,20 @@
          header.classList.remove('scrolled');
      }
  });
+ // this is for tab switching in the sponsorship.html page for the tabs
+ document.addEventListener('DOMContentLoaded', function() {
+    const tabs = document.querySelectorAll('.tab-btn');
+    const contents = document.querySelectorAll('.tab-content');
+    
+    tabs.forEach(tab => {
+        tab.addEventListener('click', () => {
+            const target = tab.dataset.tab;
+            
+            tabs.forEach(t => t.classList.remove('active'));
+            contents.forEach(c => c.classList.remove('active'));
+            
+            tab.classList.add('active');
+            document.getElementById(target).classList.add('active');
+        });
+    });
+});
