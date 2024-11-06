9f77790b9e83cd2ba83a6cb27bbb807d28bea3ca
Joshua <94143587+JoshuaO22@users.noreply.github.com>
2024-11-06 13:26:38 -0500

    Add relative file paths

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require("express")
 const app = express()
 
+// Sets up folder to serve static files
 app.use(express.static('public'));
+
+// Obscures index.html as root
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 app.get("/test.express", (req, res) => {
     console.log("Visit")
