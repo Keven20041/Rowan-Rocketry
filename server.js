9ead794e265e8b125fc1c255cf4569eae923cc86
Joshua <94143587+JoshuaO22@users.noreply.github.com>
2024-11-06 13:10:31 -0500

    Set up express js as static file server
    To use nginx as reverse proxy and make testing easier

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const app = express()
 
+app.use(express.static('public'));
+
 app.get("/test.express", (req, res) => {
     console.log("Visit")
     res.send("Hi")
