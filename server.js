9edcbf944a1a68d63eb4b0e392a8dd6c8940c865
Joshua <94143587+JoshuaO22@users.noreply.github.com>
2024-11-07 16:59:50 -0500

    Add visit logger and comments

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const app = express()
 
 const formRouter = require("./routers/forms")
 
+// Site visits logger
+app.use((req, res, next) => {
+    console.log("Visit on " + req.originalUrl + " from ip address " + req.socket.remoteAddress)
+    next()
+})
+
 // Sets up folder to serve static files, allows urls to omit the html and htm extension
 app.use(express.static('public', {extensions: ['html', 'htm']}))
 
@@ -12,17 +18,22 @@ app.use(express.static('public', {extensions: ['html', 'htm']}))
 //     res.sendFile(path.join(__dirname, 'public', 'index.html'))
 // })
 
+// Forms handling
 app.use("/forms", formRouter)
 
+// Test
 app.get("/test.express", (req, res) => {
     console.log("Visit");
-    res.status(500).json({ message: "Error"} )
+    res.status(500).json( { message: "Error"} )
 })
 
+// When file not found
 app.use((req, res, next) => {
+    console.log("404 on " + req.url + " from ip address " + req.socket.remoteAddress)
     res.status(404).sendFile(path.join(__dirname, '404.html'))
 })
 
+// Error handling
 app.use((err, req, res, next) => {
     console.log("Error!")
     next(err) // add error handling page here
