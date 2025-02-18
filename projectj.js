a0f886446d3431ce6e00c63a6ccc66f1c5d13365
Keven20041 <116309709+Keven20041@users.noreply.github.com>
2025-02-18 12:51:33 -0500

    Removed functions from previous code version of the website
    Changed the display to block instead of flex for #title-container

--- projectj.js
+++ projectj.js
@@ -88,31 +88,6 @@ document.addEventListener('DOMContentLoaded', () => {
     }
     animatePlanets();
 
-    // Debounce function for better performance
-    function debounce(func, wait) {
-        let timeout;
-        return function executedFunction(...args) {
-            const later = () => {
-                clearTimeout(timeout);
-                func(...args);
-            };
-            clearTimeout(timeout);
-            timeout = setTimeout(later, wait);
-        };
-    }
-
-    // Throttle function for better performance
-    function throttle(func, limit) {
-        let inThrottle;
-        return function(...args) {
-            if (!inThrottle) {
-                func.apply(this, args);
-                inThrottle = true;
-                setTimeout(() => inThrottle = false, limit);
-            }
-        }
-    }
-
     // Debounced resize event listener
     window.addEventListener('resize', debounce(() => {
         init();
