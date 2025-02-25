92ef27ecf4bbf0812bfcf0aa48f203fe38335a1b
Keven20041 <116309709+Keven20041@users.noreply.github.com>
2025-02-25 13:37:02 -0500

    Removed the expression from the file

--- theTeam.js
+++ theTeam.js
@@ -171,7 +171,7 @@ document.addEventListener("DOMContentLoaded", () => {
   })
 
   // Smooth scrolling for anchor links
-  document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  document.querySelectorAll('href').forEach((anchor) => {
     anchor.addEventListener("click", function (e) {
       e.preventDefault()
       const target = document.querySelector(this.getAttribute("href"))
